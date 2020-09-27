a6c8d143860e06e1b3cc13c02548b2963f960054
Magdalena Pawlak <magpaw10@st.amu.edu.pl>
2020-09-27 20:19:46 +0200

    part_2

--- rozwiazanie_zadania.py
+++ rozwiazanie_zadania.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_colwidth', None)
 
 survey_results_public_file_path = 'survey_results_public.csv'
 survey_results_public_data = pd.read_csv(survey_results_public_file_path,
-                            usecols=['Respondent','Age','WorkWeekHrs','ConvertedComp','CodeRevHrs','YearsCode'],
+                            usecols=['Respondent','Age','WorkWeekHrs','ConvertedComp','CodeRevHrs','YearsCode','Hobbyist','OpSys'],
                             index_col='Respondent')
 #print (survey_results_public_data.dtypes)
 survey_results_public_data.dropna(inplace=True)
@@ -15,19 +15,27 @@ survey_results_public_data.replace(to_replace={'Less than 1 year': '0',
                               'More than 50 years': '51'},
                              inplace=True)
 survey_results_public_data = survey_results_public_data.astype({'YearsCode': 'int64'},copy=False)
-print (survey_results_public_data.dtypes)
-print (survey_results_public_data.corr())
-
+#print (survey_results_public_data.dtypes)
+survey_results_public_data.replace(to_replace={'Yes': 1,
+                              'No': 0 },
+                             inplace=True)
+#print (survey_results_public_data.corr())
+one_hot = pd.get_dummies(survey_results_public_data['OpSys'])
+#print (one_hot)
+survey_results_public_data = survey_results_public_data.drop('OpSys', axis = 1)
+survey_results_public_data = survey_results_public_data.join(one_hot)
+print (survey_results_public_data)
 #correlated
-plt.plot(survey_results_public_data['Age'], survey_results_public_data['YearsCode'], 'ro', markersize=0.1)
-plt.title('Coding experince in accorance to the age')
-plt.xlabel('Age')
-plt.ylabel('Years of Code')
-plt.show()
+# plt.plot(survey_results_public_data['Age'], survey_results_public_data['YearsCode'], 'ro', markersize=0.1)
+# plt.title('Coding experince in accorance to the age')
+# plt.xlabel('Age')
+# plt.ylabel('Years of Code')
+# plt.show()
 #uncorrelated
-survey_results_public_data = survey_results_public_data[(survey_results_public_data['WorkWeekHrs'] <=84)]
-plt.plot(survey_results_public_data['WorkWeekHrs'], survey_results_public_data['YearsCode'], 'ro', markersize=0.1)
-plt.title('Weekly working hours in accordance to experince')
-plt.xlabel('Hours per week')
-plt.ylabel('Years of Code')
-plt.show()
+# survey_results_public_data = survey_results_public_data[(survey_results_public_data['WorkWeekHrs'] <=84)]
+# plt.plot(survey_results_public_data['WorkWeekHrs'], survey_results_public_data['YearsCode'], 'ro', markersize=0.1)
+# plt.title('Weekly working hours in accordance to experince')
+# plt.xlabel('Hours per week')
+# plt.ylabel('Years of Code')
+# plt.show()
+
