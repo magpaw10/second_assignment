a2f993b98ff0cb004e08eae2de664043cd200554
Magdalena Pawlak <magpaw10@st.amu.edu.pl>
2020-09-28 19:56:27 +0200

    third part

--- rozwiazanie_zadania.py
+++ rozwiazanie_zadania.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -24,7 +28,7 @@ one_hot = pd.get_dummies(survey_results_public_data['OpSys'])
 #print (one_hot)
 survey_results_public_data = survey_results_public_data.drop('OpSys', axis = 1)
 survey_results_public_data = survey_results_public_data.join(one_hot)
-print (survey_results_public_data)
+'''print (survey_results_public_data)'''
 #correlated
 # plt.plot(survey_results_public_data['Age'], survey_results_public_data['YearsCode'], 'ro', markersize=0.1)
 # plt.title('Coding experince in accorance to the age')
@@ -39,3 +43,84 @@ print (survey_results_public_data)
 # plt.ylabel('Years of Code')
 # plt.show()
 
+# PART 3
+# quantil method
+q1 = survey_results_public_data.quantile(0.25)
+q3 = survey_results_public_data.quantile(0.75)
+iqr = q3 - q1
+print (iqr)
+
+# buiding a summary table including numerical values of outliers (quantil method)
+low_boundary = (q1 - 1.5 * iqr)
+upp_boundary = (q3 + 1.5 * iqr)
+num_of_outliers_L = (survey_results_public_data[iqr.index] < low_boundary).sum()
+num_of_outliers_U = (survey_results_public_data [iqr.index] > upp_boundary).sum()
+outliers_15iqr = pd.DataFrame({'lower_boundary':low_boundary, 'upper_boundary':upp_boundary,'num_of_outliers_L':num_of_outliers_L, 'num_of_outliers_U':num_of_outliers_U})
+print (outliers_15iqr)
+
+# removing outliers (quantil method)
+data_without_outliers = survey_results_public_data.copy()
+print (data_without_outliers.shape[0]) #number of data before removing outliers
+for row in outliers_15iqr.iterrows():
+    data_without_outliers = data_without_outliers[(data_without_outliers[row[0]] >= row[1]['lower_boundary']) & (data_without_outliers[row[0]] <= row[1]['upper_boundary'])]
+print (data_without_outliers.shape[0]) #number of data after outliners removal  
+
+
+# standard deviation method
+elements = ['WorkWeekHrs','CodeRevHrs']
+sigma = survey_results_public_data[elements].std()
+average = survey_results_public_data[elements].mean()
+
+print(sigma)
+print (average)
+
+# buiding a summary table including numerical values of outliers (standard deviation)
+
+low_boundary = (average - 3 * sigma)
+upp_boundary = (average + 3 * sigma)
+num_of_outliers_L = (survey_results_public_data[elements] < low_boundary).sum()
+num_of_outliers_U = (survey_results_public_data[elements] > upp_boundary).sum()
+outliers_3sigma = pd.DataFrame({'lower_boundary':low_boundary, 'upper_boundary':upp_boundary,'num_of_outliers_L':num_of_outliers_L, 'num_of_outliers_U':num_of_outliers_U})
+print (outliers_3sigma)
+
+#removing outliers (standard deriviation)
+data_without_outliers_sd = survey_results_public_data.copy()
+print (data_without_outliers_sd.shape[0]) #number of data before removing outliers
+for row in outliers_3sigma.iterrows():
+    data_without_outliers_sd = data_without_outliers_sd[(data_without_outliers_sd[row[0]] >= row[1]['lower_boundary']) & (data_without_outliers_sd[row[0]] <= row[1]['upper_boundary'])]
+print (data_without_outliers_sd.shape[0]) #number of data after outliners removal  
+
+# PART 4
+# charts
+
+# box plot visualisation
+sns.boxplot(y=data_without_outliers['WorkWeekHrs'], x=data_without_outliers['CodeRevHrs'], data=data_without_outliers)
+plt.show()
+
+# linear regression visualiosation 
+sns.regplot(y=data_without_outliers['WorkWeekHrs'], x=data_without_outliers['Age'])
+plt.show()
+
+# joint plot method
+sns.jointplot(y=data_without_outliers['WorkWeekHrs'], x=data_without_outliers['Age'], data=data_without_outliers, kind='hex')
+plt.show()
+
+print (data_without_outliers)
+
+# linear regression => MODELS
+# for 1 x
+reg = linear_model.LinearRegression()
+reg.fit(data_without_outliers[['WorkWeekHrs']], data_without_outliers['CodeRevHrs'])
+print (reg.predict([[12]]))
+print (reg.predict([[40]]))
+mse = np.mean((reg.predict(data_without_outliers[['WorkWeekHrs']]) - data_without_outliers[['CodeRevHrs']])**2)
+print("Error: ", mse)
+
+# for 2 x
+reg = linear_model.LinearRegression()
+reg.fit(data_without_outliers[['WorkWeekHrs','YearsCode']], data_without_outliers[['CodeRevHrs']])
+print (reg.coef_)
+
+# 
+reg = linear_model.LinearRegression()
+reg.fit(data_without_outliers[['WorkWeekHrs','YearsCode','Age']], data_without_outliers[['CodeRevHrs']])
